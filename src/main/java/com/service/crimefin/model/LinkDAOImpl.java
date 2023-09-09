62afc80d325a47806fa470ace39f52f34f5d6a60
Austin-Choi <33799946+Austin-Choi@users.noreply.github.com>
2023-09-10 01:36:31 +0900

    [Feat] 피싱링크 관련 기능

--- src/main/java/com/service/crimefin/model/LinkDAOImpl.java
+++ src/main/java/com/service/crimefin/model/LinkDAOImpl.java
@@ -1,4 +1,4 @@
-package com.service.crimefin.model.impl;
+package com.service.crimefin.model;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
