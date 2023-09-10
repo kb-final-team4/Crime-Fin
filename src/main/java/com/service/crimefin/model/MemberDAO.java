2216fb3c39cac3d32430bdb2ee1e88f622b7fadd
Austin-Choi <33799946+Austin-Choi@users.noreply.github.com>
2023-09-10 11:14:42 +0900

    [Feat] 로그인 관련 기능

--- src/main/java/com/service/crimefin/model/MemberDAO.java
+++ src/main/java/com/service/crimefin/model/MemberDAO.java
@@ -5,12 +5,15 @@ import com.service.crimefin.domain.MemberVO;
 import java.util.List;
 
 public interface MemberDAO {
-    //0. test 메서드
-    public List<MemberVO> select();
+	public MemberVO login(MemberVO memberVO);
 
+	public int updateMemberInfo(MemberVO memberVO);
     public int insertMember(MemberVO memberVO);
     public int deleteMember(MemberVO memberVO);
-    public int updateMemberInfo(MemberVO memberVO);
-    public int updateMemberPassword(MemberVO memberVO);
-    public MemberVO login(MemberVO memberVO);
+    public String selectMemberId(MemberVO memberVO);
+
+
+    public int isExistMemberId(String memberId);
+    public int isExistMemberEmail(String email);
+    public String selectAddress(String memberId);
 }
