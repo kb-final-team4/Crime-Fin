c5891eab03f0501841bba8d5cefae141e481928d
Austin-Choi <33799946+Austin-Choi@users.noreply.github.com>
2023-09-10 11:30:55 +0900

    [Feat] 피싱링크 관련 기능

---
+++ src/main/java/com/service/crimefin/model/HistoryDAOImpl.java
@@ -0,0 +1,27 @@
+package com.service.crimefin.model.impl;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.service.crimefin.domain.HistoryVO;
+import com.service.crimefin.model.HistoryDAO;
+
+@Repository
+public class HistoryDAOImpl implements HistoryDAO{
+
+	@Autowired
+	private SqlSession sqlSession;
+	private static final String NS = "sql.crime-fin.mapper.";
+	
+	@Override
+	public String findOldPhishingIdForLink(String memberId) {
+		return sqlSession.selectOne(NS+"findOldPhishingIdForLink", memberId);
+	}
+
+	@Override
+	public int insertHistoryWithLink(HistoryVO historyVO) {
+		return sqlSession.insert(NS+"insertHistoryWithLink", historyVO);
+	}
+
+}
